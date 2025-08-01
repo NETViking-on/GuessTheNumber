b90a8ad2ab673842c9183b53f449e14312baaa52
nyxeron <ilyaprogrammer10@gmail.com>
2025-08-01 13:04:05 +0500

    Game structure and basic UI
    Add random number generation and game intro

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,6 +10,24 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            int number = rand.Next(10, 100);
+            int lowerBound = rand.Next(0, number - 5);
+            int upperBound = rand.Next(number + 1, number + 6);
+            int triesCount = 3;
+            string userInput;
+
+            Console.WriteLine("Welcome to game GuessNumber! ");
+            Console.WriteLine("Press any key to start the game!");
+            Console.ReadKey();
+            Console.WriteLine($"Try to guess number beetween {lowerBound} and {upperBound}." +
+                $"You have {triesCount} tries.Type 'exit' to quit the game.");
+
+            while (true)
+            {
+             
+            }
+
 
         }
     }
