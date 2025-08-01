c3d0a97f3b94cedd23292a1306f6c45af4b21c2c
nyxeron <ilyaprogrammer10@gmail.com>
2025-08-01 15:23:47 +0500

    feat: implement main game loop with guessing logic
    Added core gameplay logic that includes user input, number parsing, range validation, win/loss conditions, and exit command handling. The loop runs until the user guesses the number, runs out of tries, or types "exit".

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -11,24 +11,60 @@ namespace GuessTheNumber
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int number = rand.Next(10, 100);
-            int lowerBound = rand.Next(0, number - 5);
-            int upperBound = rand.Next(number + 1, number + 6);
+            int number = rand.Next(20, 80);
+            int lowerBound = rand.Next(number - 10, number - 2);
+            int upperBound = rand.Next(number + 1, number + 10);
             int triesCount = 3;
             string userInput;
 
-            Console.WriteLine("Welcome to game GuessNumber! ");
+            Console.WriteLine("Welcome to the game GuessNumber! ");
             Console.WriteLine("Press any key to start the game!");
             Console.ReadKey();
-            Console.WriteLine($"Try to guess number beetween {lowerBound} and {upperBound}." +
-                $"You have {triesCount} tries.Type 'exit' to quit the game.");
+            Console.WriteLine($"Try to guess the number between {lowerBound} and {upperBound}." +
+                $" You have {triesCount} tries. Type 'exit' to quit the game.");
 
             while (true)
             {
-             
+                Console.Write("Enter your guess: ");
+                userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "exit")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+
+                if (int.TryParse(userInput, out int guess))
+                {
+
+                    if (guess < lowerBound || guess > upperBound )
+                    {
+                        Console.WriteLine("Your answer is not in range.");
+                    }
+                    else if (guess == number)
+                    {
+                        Console.WriteLine($"Congratulations! You won! The correct answer was: {number}");
+                        break;
+                    }
+                    else
+                    {
+                        triesCount--;
+                        Console.WriteLine($"Your answer is wrong! You have {triesCount} tries" +
+                            $" Please, try again!");
+                    }
+                    if (triesCount <= 0)
+                    {
+                        Console.WriteLine($"You lost the game! Correct answer was: {number}");
+                        break;
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number or 'exit'.");
+                }
             }
-
-
+           
         }
     }
 }
